4c9f71b57dfd61f43ffe4d39ae46d5d08ac41817
Paul Crompton <paul.david.crompton@me.com>
2016-06-12 09:55:21 -0400

    Improved Pitch.transpose() Method
    Uses integers representing semitones to caluculate transposition.

--- muxmllib.py
+++ muxmllib.py
@@ -4,60 +4,28 @@ PITCHES = "CDEFGAB"
 CHROMATIC_SCALE = "C^D^EF^G^A^B"
 
 INTERVALS ={
-	'1':{
-		'dim': -1,
-		'per': 0,
-		'aug': 1
-	},
-	'2':{
-		'dim': 0,
-		'min': 1,
-		'maj': 2,
-		'aug': 3
-	},
-	'3': {
-		'dim': 2,
-		'min': 3,
-		'maj': 4,
-		'aug': 5
-	},
-	'4':{
-		'dim': 4,
-		'per': 5,
-		'aug': 6
-	},
-	'5':{
-		'dim': 6,
-		'per': 7,
-		'aug': 8
-	},
-	'6':{
-		'dim': 7,
-		'min': 8,
-		'maj': 9,
-		'aug': 10
-	},
-	'7':{
-		'dim': 9,
-		'min': 10,
-		'maj': 11,
-		'aug': 12
-	}
+	'1':{'dim': -1,'per': 0,'aug': 1},
+	'2':{'dim': 0,'min': 1,'maj': 2,'aug': 3},
+	'3': {'dim': 2,'min': 3,'maj': 4,'aug': 5},
+	'4':{'dim': 4,'per': 5,'aug': 6},
+	'5':{'dim': 6,'per': 7,'aug': 8},
+	'6':{'dim': 7,'min': 8,'maj': 9,'aug': 10},
+	'7':{'dim': 9,'min': 10,'maj': 11,'aug': 12}
 }
 
 PITCH_CLASSES = {
-	'0':['B#','C','Dbb'],
-	'1':['Bx','C#','Db'],
-	'2':['Cx','D','Ebb'],
-	'3':['D#','Eb','Fbb'],
-	'4':['Dx','E','Fb'],
-	'5':['E#','F','Gbb' ],
-	'6':['Ex','F#','Gb'],
-	'7':['Fx','G','Abb'],
-	'8':['G#','Ab'],
-	'9':['Gx','A','Bbb'],
-	'10':['A#','Bb','Cbb'],
-	'11':['Ax','B','Cb']	
+	'0':{'B': 1, 'C': 0, 'D':-2},
+	'1':{'B': 2,'C': 1,'D': -1},
+	'2':{'C': 2,'D': 0,'E': -2},
+	'3':{'D': 1,'E': -1,'F':-2},
+	'4':{'D': 2,'E': 0,'F': -1},
+	'5':{'E': 1,'F': 0,'G': -2},
+	'6':{'E': 2,'F': 1,'G': -1},
+	'7':{'F': 2,'G': 0,'A': -2},
+	'8':{'G': 1,'A': -1},
+	'9':{'G': 2,'A': 0,'B': -2},
+	'10':{'A': 1,'B': -1,'C': -2},
+	'11':{'A': 2,'B': 0,'C': -1}	
 }
 
 class Pitch():
@@ -65,22 +33,31 @@ class Pitch():
 		self.step = step
 		self.alter = int(alter)
 		self.octave = int(octave)
+		self.pitch_class = int(PITCHES.find(step)) + self.alter
+		self.semitone = 12*self.octave + self.pitch_class
 	
-	def transpose(self, direction, interval, quality=0, octave=0):
+	def transpose(self, interval, quality, octave=0):
 		
-		interval -= 1 #adjust the interval number to a more programming friendly value
-		if direction == "down":
-			interval = -interval
-		old_step = self.step
-		self.step = PITCHES[(PITCHES.find(self.step)+interval)%len(PITCHES)]
+		delta_semitone = INTERVALS[str(abs(interval))][quality]*(abs(interval)/interval)
+		new_semitone = self.semitone + delta_semitone
+		new_pitch_class = new_semitone % 12
+		delta_steps = interval + -abs(interval)/interval #adjust the interval number to a more programming friendly value
+		new_step = PITCHES[(PITCHES.find(self.step)+delta_steps)%len(PITCHES)]
+		new_alter = PITCH_CLASSES[str(new_pitch_class)][new_step]
+		new_octave = self.octave
+		if interval > 0:
+			if PITCHES.find(self.step) > PITCHES.find(new_step):
+				new_octave += 1
+		elif interval < 0:
+			if PITCHES.find(self.step) < PITCHES.find(new_step):
+				new_octave -= 1
+		new_octave += octave
 		
-		if direction == "up":
-			if PITCHES.find(old_step) > PITCHES.find(self.step):
-				self.octave += 1
-		elif direction == "down":
-			if PITCHES.find(old_step) < PITCHES.find(self.step):
-				self.octave -= 1
-		
+		self.step = new_step
+		self.alter = new_alter
+		self.octave = new_octave
+		self.pitch_class = new_pitch_class
+		self.semitone = new_semitone
 		
 
 def create_pitch(pitch):
@@ -98,15 +75,16 @@ def write_xml(dom_obj, filename):
 	file.write(xml_str)
 	file.close()
 	
-def transpose_doc(filename, direction, interval, quality=0, octaves=0):
+def transpose_doc(filename, interval, quality, octaves=0):
 	musicxml = xml.dom.minidom.parse(filename)
 	for note_node in musicxml.getElementsByTagName("note"):
 		for pitch_node in note_node.getElementsByTagName("pitch"):
 			pitch = create_pitch(pitch_node)
 			
-			#print pitch.step, pitch.alter, pitch.octave
-			pitch.transpose(direction, interval)
-			#print pitch.step, pitch.alter, pitch.octave
+			print pitch.step, pitch.alter, pitch.octave
+			pitch.transpose(interval, quality)
+			print pitch.step, pitch.alter, pitch.octave
+			print
 			
 			pitch_node.getElementsByTagName("step")[0].firstChild.data = pitch.step
 			try:
@@ -115,7 +93,12 @@ def transpose_doc(filename, direction, interval, quality=0, octaves=0):
 				pass
 			pitch_node.getElementsByTagName("octave")[0].firstChild.data = pitch.octave
 			
-			write_xml(musicxml, filename.split('.')[0]+direction+str(interval)+str(quality)+str(octaves)+".xml")
+			if interval < 0:
+				direction = 'down'
+			else:
+				direction = 'up'
+			
+			write_xml(musicxml, 'output/'+filename.split('.')[0]+'_'+direction+str(quality)+str(abs(interval))+str(octaves)+".xml")
 			
 
 
